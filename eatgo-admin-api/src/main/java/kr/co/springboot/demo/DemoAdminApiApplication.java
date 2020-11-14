1418d775061e55bbc6e824984219c301e495b836
koreanstudent <hn12344@naver.com>
2020-11-14 18:01:56 +0900

    spring boot   - 프로젝트 분리 1     - admin, common 생성

--- eatgo-admin-api/src/main/java/kr/co/springboot/demo/DemoAdminApiApplication.java
+++ eatgo-admin-api/src/main/java/kr/co/springboot/demo/DemoAdminApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoAdminApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoAdminApiApplication.class, args);
 	}
 
 }
